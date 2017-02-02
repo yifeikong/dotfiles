b2818f768c111ca7ae17822d7dc9614a22bb0818
Yifei Kong <kongyifei@gmail.com>
2017-02-02 20:15:06 +0800

    update nginx install script

--- install_nginx_ubuntu.sh
+++ install_nginx_ubuntu.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
-NGINX_VER=1.11.4
+echo which nginx version to install
+read NGINX_VER 
+echo where to install nginx
+read INSTALL_DIR 
+
+if [ -z $NGINX_VER ]; then
+    NGINX_VER=1.11.4
+fi
+
+if [ -z $INSTALL_DIR ]; then
+    INSTALL_DIR=/opt/spider/nginx
+fi
 
 wget https://nginx.org/download/nginx-${NGINX_VER}.tar.gz
 tar xvzf nginx-${NGINX_VER}.tar.gz
-cd nginx-$NGINX_VER
+cd nginx-${NGINX_VER}
 apt-get install libpcre3-dev zlib1g-dev
 ./configure \
-    --prefix=/opt/nginx
+    --prefix=${INSTALL_DIR}
 make
 sudo make install
 cd -
-rm -rf nginx-$NGINX_VER
+rm -rf nginx-${NGINX_VER}
